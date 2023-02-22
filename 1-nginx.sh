25b016b5c84121118bad17f23752bbc6fac7e90a
tmmarlyn <123255749+tmmarlyn@users.noreply.github.com>
2023-02-22 20:49:27 -0300

    Actualización con mejoras de comentarios

--- 1-nginx.sh
+++ 1-nginx.sh
@@ -6,13 +6,23 @@
 
 # Go to Docker Hub y busque la imagen de Nginx. Descarga la version 1.22.1
 
-echo "Inicio del Script"
+echo "---------------------- Inicio del Script -----------------------"
 V_IMAGE=$1
 
-echo "Descarga de la imagen: $V_IMAGE"
+echo " "
+echo "*************** Descarga de la imagen: $V_IMAGE ***************"
 docker pull $V_IMAGE
 
-echo "Se ejecuta la imagen: $V_IMAGE"
+echo " "
+echo "*************** Se ejecuta la imagen: $V_IMAGE ****************"
 docker run -d --name nginx-web -p 8080:80 $V_IMAGE
 
-echo "Fin del script"
+echo " "
+echo "************** Verificamos imagen y contenedor ****************"
+docker images
+
+echo " "
+docker ps
+
+echo " "
+echo "----------------------- Fin del script -------------------------"
